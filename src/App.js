293df7f58a1c7bca06996d4d2c4741953befc5c9
Leonids Jefimovs <72824492+stealthchopper@users.noreply.github.com>
2021-05-31 15:59:46 +0100

    changed message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsasshole</code> and save to reload.
+          <code>Dima Is Gay</code>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jigaboo
         </a>
       </header>
     </div>
